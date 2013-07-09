a7896a5cd481082e944418981faa4c1d06cd7e71
Rémy Coutable <remy@rymai.me>
2013-07-09 14:36:31 +0200

    Fix shit and add specs

--- app/workers/log_reader_worker.rb
+++ app/workers/log_reader_worker.rb
@@ -20,14 +20,11 @@ class LogReaderWorker
   end
 
   def _read_log_and_delay_gif_requests_parsing
-    logger.info '_read_log_and_delay_gif_requests_parsing...'
     _gif_request_lines { |line| LogLineParserWorker.perform_async(line) }
   end
 
   def _gif_request_lines
-    logger.info '_gif_request_lines...'
     _gzip_lines do |line|
-      logger.info "line is gif request?: #{_gif_request?(line)}"
       yield(line) if _gif_request?(line)
     end
   end
@@ -39,20 +36,24 @@ class LogReaderWorker
   def _gzip_lines
     _log_file do |tempfile|
       gz = Zlib::GzipReader.new(tempfile, encoding: 'iso-8859-1')
-      logger.info "GZip file: #{gz.inspect}"
       gz.each_line { |line| yield(line) }
     end
   end
 
   def _log_file
-    fog_file = S3Wrapper.fog_connection.get_object(S3Wrapper.bucket, _log_filename)
-    tempfile = Tempfile.new(['export', '.csv'])
-    tempfile.write(fog_file.body)
-    tempfile.rewind
-    yield(tempfile)
-  ensure
-    tempfile.close
-    tempfile.unlink
+    tempfile = Tempfile.new([File.basename(_log_filename), '.gz'])
+    begin
+      tempfile.write(_fog_file.body.encode!('UTF-8', 'UTF-8', invalid: :replace))
+      tempfile.rewind
+      yield(tempfile)
+    ensure
+      tempfile.close
+      tempfile.unlink
+    end
+  end
+
+  def _fog_file
+    @_fog_file ||= S3Wrapper.fog_connection.get_object(S3Wrapper.bucket, _log_filename)
   end
 
   def _log_filename
---
+++ spec/workers/log_reader_worker_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe LogReaderWorker do
+  let(:fog_file) { double('fog_file', body: fixture_file('logs/voxcast/4076.voxcdn.com.log.1355880780-1355880840.gz').read) }
+  let(:worker) { described_class.new }
+
+  describe '#perform' do
+    before do
+      expect(worker).to receive(:_fog_file) { fog_file }
+    end
+
+    it 'delays LogLineParserWorker for each line in the log file' do
+      expect(LogLineParserWorker).to receive(:perform_async).at_least(500).times
+
+      worker.perform(Time.at(1355880780).to_s)
+
+      expect(worker.send(:_log_filename)).to eq('voxcast/4076.voxcdn.com.log.1355880780-1355880840.gz')
+    end
+  end
+
+end
