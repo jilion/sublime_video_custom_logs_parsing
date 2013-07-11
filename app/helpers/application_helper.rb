368a10e68b5f30119f2bb132b1d72561d1fbd099
Rémy Coutable <remy@rymai.me>
2013-07-11 11:26:06 +0200

    Improve and refactor charts / helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,46 +4,12 @@ module ApplicationHelper
     number_with_delimiter(number, options)
   end
 
-  def monthly_views_per_countries_for_chart(monthly_views_per_countries, start_day, end_day)
-    monthly_views_per_countries = _fill_missing_months(monthly_views_per_countries, start_day, end_day)
-
-    monthly_views_per_country_names = {}
-    monthly_views_per_countries.each do |country_key, monthly_views|
-      if country = Country[country_key]
-        monthly_views_per_country_names[country.name] = monthly_views
-      end
-    end
-
-    Hash[monthly_views_per_country_names.sort { |a, b| b[1].sum <=> a[1].sum }.slice(0, 15)]
+  def monthly_views_per_country_for_chart(monthly_views_per_countries, start_day, end_day)
+    Hash[_monthly_views_per_country(monthly_views_per_countries, start_day, end_day).sort { |a, b| b[1].sum <=> a[1].sum }.slice(0, 15)]
   end
 
-  def monthly_views_per_subregions_for_chart(monthly_views_per_countries, start_day, end_day)
-    monthly_views_per_countries = _fill_missing_months(monthly_views_per_countries, start_day, end_day)
-
-    monthly_views_per_country_names = {}
-    monthly_views_per_countries.each do |country_key, monthly_views|
-      if country = Country[country_key]
-        monthly_views_per_country_names[country.region] ||= []
-        monthly_views.each_with_index do |v, i|
-          monthly_views_per_country_names[country.region][i] ||= 0
-          monthly_views_per_country_names[country.region][i]  += v
-        end
-      end
-    end
-
-    Hash[monthly_views_per_country_names.sort { |a, b| b[1].sum <=> a[1].sum }]
-  end
-
-  def views_per_month(chart_data)
-    monthly_views ||= Hash.new(0)
-
-    chart_data.map do |country_hash|
-      country_hash[:data].each do |day, views|
-        monthly_views[day] += views
-      end
-    end
-
-    monthly_views
+  def monthly_views_per_region_for_chart(monthly_views_per_countries, start_day, end_day)
+    Hash[_monthly_views_per_region(monthly_views_per_countries, start_day, end_day).sort { |a, b| b[1].sum <=> a[1].sum }]
   end
 
   private
@@ -61,6 +27,40 @@ module ApplicationHelper
     end
 
     monthly_views_per_countries_with_missing_day_filled
+  end
+
+  def _monthly_views_per_country(monthly_views_per_countries, start_day, end_day)
+    monthly_views_per_country = {}
+    _fill_missing_months(monthly_views_per_countries, start_day, end_day).each do |country_key, monthly_views|
+      _group_by_country(monthly_views_per_country, country_key, monthly_views)
+    end
+
+    monthly_views_per_country
+  end
+
+  def _monthly_views_per_region(monthly_views_per_countries, start_day, end_day)
+    monthly_views_per_region = {}
+    _fill_missing_months(monthly_views_per_countries, start_day, end_day).each do |country_key, monthly_views|
+      _group_by_region(monthly_views_per_region, country_key, monthly_views)
+    end
+
+    monthly_views_per_region
+  end
+
+  def _group_by_country(monthly_views_per_country, country_key, monthly_views)
+    if country = Country[country_key]
+      monthly_views_per_country[country.name] = monthly_views
+    end
+  end
+
+  def _group_by_region(monthly_views_per_region, country_key, monthly_views)
+    if country = Country[country_key]
+      monthly_views_per_region[country.region] ||= []
+      monthly_views.each_with_index do |v, i|
+        monthly_views_per_region[country.region][i] ||= 0
+        monthly_views_per_region[country.region][i]  += v
+      end
+    end
   end
 
 end
