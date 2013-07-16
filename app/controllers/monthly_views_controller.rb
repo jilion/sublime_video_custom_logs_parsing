670241f4f3418d0db230bb872dc7560e877d904e
Rémy Coutable <remy@rymai.me>
2013-07-17 00:16:37 +0200

    .to_date

--- app/controllers/monthly_views_controller.rb
+++ app/controllers/monthly_views_controller.rb
@@ -13,8 +13,7 @@ class MonthlyViewsController < ApplicationController
   private
 
   def _set_dates
-    # @start_day = DailyViewsPerCountry.order(:day).first.day.beginning_of_month
-    @start_day = Time.utc(2012, 9)
+    @start_day = Time.utc(2012, 9).to_date
     @end_day   = Time.now.utc
   end
 
